89e511f9717d4e400144b1898c44584a0e38b742
Parsa Yadollahi <parsayadollahi@gmail.com>
2019-02-07 11:18:05 -0500

    fixed the sorting an parallel

--- Lecture5/sortingandparalell.java
+++ Lecture5/sortingandparalell.java
@@ -6,24 +6,26 @@ public class sortingandparalell {
 
 	public static void main(String[] args) {
 		int[] arr1 = {1,2,3};
-		int[] arr2 = {4,2,6};
-		System.out.println(sortingParallelPointers(arr1, arr2));
+		int[] arr2 = {4,2,3,5,1};
+		System.out.println("The two arrays share " + sortingParallelPointers(arr1, arr2) + " elements in common");
 	}
-	public static boolean sortingParallelPointers(int[] arr1, int[] arr2){
+	public static int sortingParallelPointers(int[] arr1, int[] arr2){
 		Arrays.sort(arr1);
 		Arrays.sort(arr2);
-		for(int i = 0; i < arr1.length-1; i++) {
-// need to reset second array's index
-			int j = 0;
-// loop through elements in second array
-			while (j < arr2.length) {
-				if( arr1[i] == arr2[j]) {
-					return true;	
-				} else {
-					j++;
-				}
+		int i = 0;
+		int j = 0;
+		int sum = 0;
+		while (i < arr1.length && j < arr2.length) {
+			if (arr1[i] == arr2 [j]) {
+				sum++;
+				i++;
+				j++;
+			} else if (arr1[i] < arr2[j]) {
+				i++;
+			} else {
+				j++;
 			}
 		}
-		return false;
+		return sum;
 	}
 }
