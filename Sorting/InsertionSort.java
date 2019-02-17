b90a8ade82b58c8bcefbd6f54e7b965b0dd2e97f
Parsa Yadollahi <parsayadollahi@gmail.com>
2019-02-17 02:33:03 -0500

    sorting algs

---
+++ Sorting/InsertionSort.java
@@ -0,0 +1,41 @@
+package Sorting;
+
+import java.util.*;
+public class InsertionSort {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> list = new ArrayList<>();
+		for (int i = 10; i >= 0; i--) {
+			list.add(i);
+		}
+		list.add(5);
+		list.add(200);
+		list.add(9);
+		list.add(6);
+		list.add(2);
+		list.add(14);
+		list.add(7);
+		int n = list.size();
+		for (int i = 1; i < n; i++) {
+			int element = list.get(i);
+			int j = i-1;
+			while ( j >= 0) {
+				if (element < list.get(0)) {
+					list.remove(i);
+					list.add(0, element);
+					break;
+				}
+				if (element >= list.get(j)) {
+					list.remove(i);
+					list.add(j+1, element);
+					break;
+				}
+				if (element < list.get(j)) {
+					j--;
+				}
+			}
+		}
+		System.out.println(list);
+	}
+
+}
---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,34 @@
+package Sorting;
+
+import java.util.*;
+
+public class SelectionSort {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> list = new ArrayList<>();
+
+		for (int i = 10; i >= 0; i--) {
+			list.add(i);
+		}
+		int n = list.size();
+		for (int i = 0; i < n-1; i++) {
+			int min = i;
+			for(int j = i; j < n; j++) {
+				if ( list.get(j) < list.get(min)) {
+					min = j;
+				}
+			}
+			swap(list, i, min);
+		}
+		System.out.println(list);
+	}
+		
+	public static void swap(ArrayList<Integer> list, int first, int second) {
+		int temp = list.get(first);
+		list.add(first, list.get(second));
+		list.remove(first+1);
+		list.add(second,temp);
+		list.remove(second+1);
+	}
+
+}
