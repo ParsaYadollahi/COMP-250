0c2a205eee2a404ce99798e55545f20a82d8c8f3
Parsa Yadollahi <parsa.yadollahi@mail.mcgill.ca>
2019-12-07 15:15:49 -0500

    changing laptops

--- Assignment4/src/A4/Multiply.java
+++ Assignment4/src/A4/Multiply.java
@@ -9,22 +9,75 @@ public class Multiply{
         int maxval = (1 << size) - 1;
         return rand.nextInt(maxval + 1);
     }
-    
-    public static int[] naive(int size, int x, int y) {
 
+    public static int[] naive(int size, int x, int y) {
+        int[] result = new int[2];
+        int m, a, b, c, d, e, f, g, h = 0;
         // YOUR CODE GOES HERE  (Note: Change return statement)
-        
-        
-        return null;
-        
+        if (size == 1) { // Base case
+            result[0] = x * y;
+            result[1] = 1;
+            return result;
+        } else {
+            m = (int) Math.ceil((size / 2.0)); // cut size by half
+            int power_2 = (int) (Math.pow(2, m)); // move bits by m/2
+            int power_2m =   (int) (Math.pow(2, 2*m)); // move bits by m
+            a = (int) Math.floor(x/power_2);
+            c = (int) Math.floor(y/power_2);
+            b = x % power_2;
+            d = y % power_2;
+            // Grade school math
+            int temp1[] = naive(m, a, c); // ac
+            int temp2[] = naive(m, b ,d); // bd
+            int temp3[] = naive(m, b, c); // bc
+            int temp4[] = naive(m, a, d); // ad
+
+            // Store results
+            e = temp1[0];
+            f = temp2[0];
+            g = temp3[0];
+            h = temp4[0];
+
+            result[0] = power_2m*e + power_2*(g+h)+f;
+            result[1] = 3*m + temp1[1] + temp2[1] + temp2[1] + temp3[1] + temp4[1];
+
+        }
+        return result;
     }
 
     public static int[] karatsuba(int size, int x, int y) {
-        
+
         // YOUR CODE GOES HERE  (Note: Change return statement)
-        
-        return null;
-        
+        int m, a, b, c, d, e, f, g = 0;
+        int result[] = new int[2];
+
+        if (size == 1){ // Base case
+            result[0] = x*y;
+            result[1] = 1;
+            return result;
+        } else {
+            m = (int) Math.floor(size / 2.0); // cut size by half
+            int power_2 = (int) (Math.pow(2, m)); // move bits by m/2
+            int power_2m =   (int) (Math.pow(2, 2*m)); // move bits by m
+            a = (int) Math.floor(x / power_2);
+            c = (int) Math.floor(y / power_2);
+            b = x % power_2;
+            d = y % power_2;
+            int[] temp1 = karatsuba(m, a, c); // ac
+            int[] temp2 = karatsuba(m, b,d); // bd
+            int[] temp3 = karatsuba(m, (a-b), (c-d)); // +(a-b) + (c-d)
+
+            // store results
+            e = temp1[0];
+            f = temp2[0];
+            g = temp3[0];
+
+            result[0] = power_2m*e + (power_2*(e+f-g) + f);
+            result[1] = 6*m+temp1[1] + temp2[1] + temp3[1];
+
+
+        }
+        return result;
     }
     
     public static void main(String[] args){
